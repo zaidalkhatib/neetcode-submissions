15cac6eb639b89b270a7d2a3a5eff97ca8175946
Zaid Alkhatib <61860288+zaidalkhatib@users.noreply.github.com>
2026-08-29 16:42:04 -0700

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        Map<Integer,List<Integer>> graph = new HashMap<>();
+        Map<Integer,Integer> inDegree = new HashMap<>();
+        for(int i=0 ;i<numCourses ;i++){
+            graph.put(i,new ArrayList<>());
+            inDegree.put(i,0);
+        }
+
+        for(int [] list : prerequisites){
+            int child = list[0];
+            int parent = list[1];
+            graph.get(parent).add(child);
+        }
+        for(int parent : graph.keySet()){
+            for(int child : graph.get(parent)){
+                inDegree.put(child,inDegree.get(child)+1);
+            }
+        }
+        Queue<Integer> queue = new LinkedList();
+        for(int child : inDegree.keySet()){
+            if(inDegree.get(child)==0){
+                queue.add(child);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+        while(!queue.isEmpty()){
+            int curr = queue.remove();
+            list.add(curr);
+            for(int child : graph.get(curr)){
+                inDegree.put(child,inDegree.get(child) - 1);
+                if(inDegree.get(child)== 0){
+                    queue.add(child);
+                }
+            }
+        }
+        int [] res = new int [list.size()];
+        for(int i=0 ;i<list.size() ;i++){
+            res[i]=list.get(i);
+        }
+        return list.size()==numCourses?res:new int []{};
+    }
+}
