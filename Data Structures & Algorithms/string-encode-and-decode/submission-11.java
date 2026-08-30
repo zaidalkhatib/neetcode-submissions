f4b90dd1f13bcceb8c602c75aaa0aa22279bc4bb
Zaid Alkhatib <61860288+zaidalkhatib@users.noreply.github.com>
2026-08-30 14:08:41 -0700

    Add: string-encode-and-decode - submission-11

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-11.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length() + "").append("#").append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res = new ArrayList<>();
+        System.out.println(str + " " + str.length());
+        int i = 0;
+        int prevEndPosition = -1;
+        while (i < str.length()) {
+            if (str.charAt(i) == '#') {
+                int j = i - 1;
+                String numAsStr="";
+                while (prevEndPosition < j) {
+                    //num = 10 * num + (str.charAt(j) - '0');
+                    numAsStr=str.charAt(j)+numAsStr;
+                    j--;
+                }
+                int num=Integer.parseInt(numAsStr);
+                i++;
+                prevEndPosition=i+num-1;
+                System.out.println("num "+ num+ " prevEndPosition " + prevEndPosition+ " " + str.substring(i, (i + num)));
+                res.add(str.substring(i, (i + num)));
+                i += num;
+            } else {
+                i++;
+            }
+        }
+        return res;
+    }
+}
+
+/*
+#5
+Hello","World,3#zaid4#
+6#hello35#World8#3#zaid4#
+
+*/
